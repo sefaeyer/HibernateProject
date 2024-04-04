ff88aa8189dbceaa03410b349e0bf0ab3cb1b1f9
Sefa Eyer <eyersefa@gmail.com>
2024-04-04 23:27:59 +0300

    Initial commit

---
+++ src/main/java/com/hb01/annotations/Student01.java
@@ -0,0 +1,27 @@
+package com.hb01.annotations;
+
+import javax.persistence.*;
+
+@Entity  //!!! @Entity koyduğumuz sınıf, veritabanında bir tabloya karşılık gelecek
+@Table(name = "t_student01") //!!! Eğer oluşacak tablo ismini değiştirmek istenirse name attribute ile
+                             // bir tablo ismi verilir.
+//!!! @Table ile isim değiştirilmese idi, DB de (küçük harfle) student01 isminde tablo oluşacaktı
+public class Student01 { // student01 isimli tablo olusur
+
+    @Id // !!! @Id -> primary key olusmasini sagliyor
+    //@Column(name = "std_id")
+    private int id;
+
+    //!!! Column annotationı zorunlu değil ancak customize edebilmek için  gerekli
+    @Column(name = "student_name", length = 100, nullable = false, unique = false )
+    //!!! Javaca konusurken name , SQL ce konusacaksam student_name kullanmam lazim
+    private String name;
+
+    //!!! @Transient --> veritabanı tablosunda "grade" adında bir kolon oluşturulmaz. kod içinde
+    // kullanılmak için bir değişken oluştururuz ama bu sınıf DB ye kaydedileceği
+    // zaman ilgili tabloya kolon olarak eklenmez
+    //@Transient
+    private int grade;
+
+
+}
